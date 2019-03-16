a9e2d4e1620c0ddef84ab4f309b4b361eca9c164
Howard Su <howard0su@gmail.com>
2019-03-16 10:16:48 +0800

    Add qfp lib to enable GCC-8 build

--- Makefile
+++ Makefile
@@ -1,25 +1,58 @@
+ifdef USE_GCC
+	CC	= arm-none-eabi-gcc
+	CXX 	= arm-none-eabi-g++
+	ASM 	= arm-none-eabi-as
+	LD 		= arm-none-eabi-gcc
+	OBJCOPY = arm-none-eabi-objcopy
+	SIZE    = arm-none-eabi-size
+else
+	CC 		= armcc --c99
+	CXX 	= armcc  --cpp
+	ASM 	= armasm
+	LD 	= armlink
+	OBJCOPY = fromelf
+endif
 
 SDIR = .
-VPATH = $(SDIR)/Silverware/src:$(SDIR)/Utilities:$(SDIR)/Libraries/STM32F0xx_StdPeriph_Driver/src:$(SDIR)/Libraries/CMSIS/Device/ST/STM32F0xx/Source/Templates/arm
 SRC_C = $(wildcard $(SDIR)/Silverware/src/*.c) \
-	$(wildcard $(SDIR)/Utilities/*.c) \
-	$(wildcard $(SDIR)/Libraries/STM32F0xx_StdPeriph_Driver/src/*.c)
+		$(wildcard $(SDIR)/Utilities/*.c) \
+		$(wildcard $(SDIR)/Libraries/STM32F0xx_StdPeriph_Driver/src/*.c)
 SRC_CXX = $(wildcard $(SDIR)/Silverware/src/*.cpp)
-SRC_S = $(SDIR)/Libraries/CMSIS/Device/ST/STM32F0xx/Source/Templates/arm/startup_stm32f031.s
 
-CFLAGS := -I$(DIR)/Silverware/src -I$(SDIR)/Libraries/CMSIS/Device/ST/STM32F0xx/Include -I $(SDIR)/Libraries/CMSIS/Include -I $(SDIR)/Utilities -I $(SDIR)/Libraries/STM32F0xx_StdPeriph_Driver/inc
+ifdef USE_GCC
+SRC_C += $(wildcard $(SDIR)/gcc/*.c)
+SRC_S += $(SDIR)/Libraries/CMSIS/Device/ST/STM32F0xx/Source/Templates/gcc_ride7/startup_stm32f030.s
+SRC_S += $(SDIR)/gcc/qfplib/qfplib.s
+LDFLAGS := -Wl,-wrap,__aeabi_dmul \
+	   -Wl,-wrap,__aeabi_fadd \
+	   -Wl,-wrap,__aeabi_fdiv \
+	   -Wl,-wrap,__aeabi_fmul \
+	   -Wl,-wrap,__aeabi_fsub \
+	   -Wl,-wrap,__aeabi_i2f  \
+	   -Wl,-wrap,__aeabi_ui2f \
+	   -Wl,-wrap,__aeabi_f2iz \
+	   -Wl,-wrap,__aeabi_f2uiz \
+	   -Wl,-wrap,__aeabi_fcmpeq \
+	   -Wl,-wrap,__aeabi_fcmplt \
+	   -Wl,-wrap,__aeabi_fcmple \
+	   -Wl,-wrap,__aeabi_fcmpge \
+	   -Wl,-wrap,__aeabi_fcmpgt \
+	   -Wl,-wrap,__aeabi_fcmpun
+else
+SRC_S += $(SDIR)/Libraries/CMSIS/Device/ST/STM32F0xx/Source/Templates/arm/startup_stm32f031.s
+endif
 
-CPU = --cpu Cortex-M0
+INCLUDES = -I$(SDIR)/Silverware/src -I$(SDIR)/Libraries/CMSIS/Device/ST/STM32F0xx/Include -I $(SDIR)/Libraries/CMSIS/Include -I $(SDIR)/Utilities -I $(SDIR)/Libraries/STM32F0xx_StdPeriph_Driver/inc
 
-CFLAGS   += $(CPU) -D__EVAL -D__MICROLIB -g -O2 --apcs=interwork --split_sections -D__UVISION_VERSION="524" -DUSE_STDPERIPH_DRIVER -DSTM32F031 --fpmode=fast
-ASMFLAGS := $(CPU) --pd "__EVAL SETA 1" -g --apcs=interwork --pd "__MICROLIB SETA 1" --pd "__UVISION_VERSION SETA 524" --xref
-LDFLAGS  := $(CPU) --library_type=microlib --ro-base 0x08000000 --entry 0x08000000 --rw-base 0x20000000 --entry Reset_Handler --first __Vectors --strict --info summarysizes
+CPU = cortex-m0
 
-SRCS     := $(SRC_C) $(SRC_CXX) $(SRC_S)
+SRCS     = $(SRC_C) $(SRC_CXX) $(SRC_S)
 ODIR     = $(SDIR)/obj
 
 OBJS 	 = $(addprefix $(ODIR)/, $(notdir $(SRC_C:.c=.o) $(SRC_S:.s=.o) $(SRC_CXX:.cpp=.o)))
+VPATH 	 = $(dir $(SRCS))
 
+ifndef USE_GCC
 export ARM_TOOL_VARIANT = mdk_lite
 export ARMCC5_ASMOPT = --diag_suppress=9931
 export ARMCC5_CCOPT = --diag_suppress=9931
@@ -28,6 +61,23 @@ export CPU_TYPE = STM32F030F4
 export CPU_VENDOR = STMicroelectronics
 export CPU_CLOCK = 0x00B71B00
 export UV2_TARGET = BWhoop
+endif
+
+
+ifndef USE_GCC
+CFLAGS   := --cpu $(CPU) $(INCLUDES) -D__EVAL -D__MICROLIB -g -O2 --apcs=interwork --split_sections -D__UVISION_VERSION="524" -DUSE_STDPERIPH_DRIVER -DSTM32F031 --fpmode=fast
+ASMFLAGS := --cpu $(CPU) --pd "__EVAL SETA 1" -g --apcs=interwork --pd "__MICROLIB SETA 1" --pd "__UVISION_VERSION SETA 524" --xref
+LDFLAGS  := --cpu $(CPU) --library_type=microlib --ro-base 0x08000000 --entry 0x08000000 --rw-base 0x20000000 --entry Reset_Handler --first __Vectors --strict --info summarysizes
+DEPENDS  := --depend=
+else
+CFLAGS   := -mcpu=$(CPU) $(INCLUDES) -DDISABLE_GESTURES2 -Os -g -mthumb -fdata-sections -ffunction-sections \
+            -fsingle-precision-constant -ffast-math \
+            -nostartfiles --specs=nano.specs --specs=nosys.specs
+CFLAGS   += -DUSE_STDPERIPH_DRIVER -DSTM32F031
+LDFLAGS  += $(CFLAGS) -Wl,-T,flash.ld,-Map,output.map,--gc-sections -L$(SDIR)/gcc
+ASMFLAGS := -mcpu=$(CPU)
+DEPENDS  := -MD -MP -MF 
+endif
 
 .PHONY: default all
 default: silverware.hex
@@ -41,21 +91,28 @@ $(ODIR):
 
 $(ODIR)/%.o: %.cpp
 	@echo " + Compiling '$(notdir $<)'"
-	armcc --cpp $(CFLAGS) --depend=$(@:.o=.dep) -c -o $@ $<
+	$(CXX) $(CFLAGS) $(DEPENDS)$(@:.o=.dep) -c -o $@ $<
 
 $(ODIR)/%.o: %.c
 	@echo " + Compiling '$(notdir $<)'"
-	armcc --c99 $(CFLAGS) --depend=$(@:.o=.dep) -c -o $@ $<
+	$(CC) $(CFLAGS) $(DEPENDS)$(@:.o=.dep) -c -o $@ $<
 
 $(ODIR)/%.o: %.s
 	@echo " + Compiling '$(notdir $<')"
-	armasm $(ASMFLAGS) -o $@ $<
+	$(ASM) $(ASMFLAGS) -o $@ $<
 
 silverware.hex: silverware.axf
+ifdef USE_GCC
+	$(OBJCOPY) -O ihex $< $@
+else
 	fromelf $< --i32combined --output $@
+endif
 
 silverware.axf: $(OBJS)
-	armlink $(LDFLAGS) $(OBJS) -o $@
+	$(LD) $(LDFLAGS) $(OBJS) -o $@
+ifdef USE_GCC
+	$(SIZE) $@
+endif
 
 clean:
 	rm -Rf $(ODIR) silverware.axf silverware.hex
--- gcc/methods.c
+++ gcc/methods.c
@@ -1,1 +1,179 @@
-void _sbrk() {}
+#include "qfplib/qfplib.h"
+
+///////////////////////////////////////////////////////////////////////////////
+//  Table 3, single precision floating-point comparison helper functions
+
+//  qfp_fcmp(r0, r1):
+//  equal? return 0
+//  r0 > r1? return +1
+//  r0 < r1: return -1
+
+//  result (1, 0) denotes (=, ?<>) [2], use for C == and !=
+int __wrap___aeabi_fcmpeq(float x, float y) {
+    return (qfp_fcmp(x, y) == 0)  //  x == y
+        ? 1 : 0;
+}
+//  Unit Tests:
+//  aeabi_fcmpeq(2205.196, 2205.196) = 1
+//  aeabi_fcmpeq(2205.196, 2205.195) = 0
+//  aeabi_fcmpeq(2205.196, 2205.197) = 0
+//  aeabi_fcmpeq(2205.196, 0) = 0
+//  aeabi_fcmpeq(-2205.196, -2205.196) = 1
+//  aeabi_fcmpeq(-2205.196, -2205.195) = 0
+//  aeabi_fcmpeq(-2205.196, -2205.197) = 0
+//  aeabi_fcmpeq(-2205.196, 0) = 0
+
+//  result (1, 0) denotes (<, ?>=) [2], use for C <
+int __wrap___aeabi_fcmplt(float x, float y) {
+    return (qfp_fcmp(x, y) < 0)  //  x < y
+        ? 1 : 0;
+}
+//  Unit Tests:
+//  aeabi_fcmplt(2205.196, 2205.196) = 0
+//  aeabi_fcmplt(2205.196, 2205.195) = 0
+//  aeabi_fcmplt(2205.196, 2205.197) = 1
+//  aeabi_fcmplt(2205.196, 0) = 0
+//  aeabi_fcmplt(-2205.196, -2205.196) = 0
+//  aeabi_fcmplt(-2205.196, -2205.195) = 1
+//  aeabi_fcmplt(-2205.196, -2205.197) = 0
+//  aeabi_fcmplt(-2205.196, 0) = 1
+
+//  result (1, 0) denotes (<=, ?>) [2], use for C <=
+int __wrap___aeabi_fcmple(float x, float y) { 
+    return (qfp_fcmp(x, y) > 0)  //  x > y
+        ? 0 : 1; 
+}
+//  Unit Tests:
+//  aeabi_fcmple(2205.196, 2205.196) = 1
+//  aeabi_fcmple(2205.196, 2205.195) = 0
+//  aeabi_fcmple(2205.196, 2205.197) = 1
+//  aeabi_fcmple(2205.196, 0) = 0
+//  aeabi_fcmple(-2205.196, -2205.196) = 1
+//  aeabi_fcmple(-2205.196, -2205.195) = 1
+//  aeabi_fcmple(-2205.196, -2205.197) = 0
+//  aeabi_fcmple(-2205.196, 0) = 1
+
+//  result (1, 0) denotes (>=, ?<) [2], use for C >=
+int __wrap___aeabi_fcmpge(float x, float y) { 
+    return (qfp_fcmp(x, y) < 0)  //  x < y
+        ? 0 : 1; 
+}
+//  Unit Tests:
+//  aeabi_fcmpge(2205.196, 2205.196) = 1
+//  aeabi_fcmpge(2205.196, 2205.195) = 1
+//  aeabi_fcmpge(2205.196, 2205.197) = 0
+//  aeabi_fcmpge(2205.196, 0) = 1
+//  aeabi_fcmpge(-2205.196, -2205.196) = 1
+//  aeabi_fcmpge(-2205.196, -2205.195) = 0
+//  aeabi_fcmpge(-2205.196, -2205.197) = 1
+//  aeabi_fcmpge(-2205.196, 0) = 0
+
+//  result (1, 0) denotes (>, ?<=) [2], use for C >
+int __wrap___aeabi_fcmpgt(float x, float y) { 
+    return (qfp_fcmp(x, y) > 0)  //  x > y
+        ? 1 : 0; 
+}
+//  Unit Tests:
+//  aeabi_fcmpgt(2205.196, 2205.196) = 0
+//  aeabi_fcmpgt(2205.196, 2205.195) = 1
+//  aeabi_fcmpgt(2205.196, 2205.197) = 0
+//  aeabi_fcmpgt(2205.196, 0) = 1
+//  aeabi_fcmpgt(-2205.196, -2205.196) = 0
+//  aeabi_fcmpgt(-2205.196, -2205.195) = 0
+//  aeabi_fcmpgt(-2205.196, -2205.197) = 1
+//  aeabi_fcmpgt(-2205.196, 0) = 0
+
+//  result (1, 0) denotes (?, <=>) [2], use for C99 isunordered()
+int __wrap___aeabi_fcmpun(float x, float y) { 
+    return (qfp_fcmp(x, y) == 0)  //  x == y
+        ? 0 : 1;
+}
+//  Unit Tests:
+//  aeabi_fcmpun(2205.196, 2205.196) = 0
+//  aeabi_fcmpun(2205.196, 2205.195) = 1
+//  aeabi_fcmpun(2205.196, 2205.197) = 1
+//  aeabi_fcmpun(2205.196, 0) = 1
+//  aeabi_fcmpun(-2205.196, -2205.196) = 0
+//  aeabi_fcmpun(-2205.196, -2205.195) = 1
+//  aeabi_fcmpun(-2205.196, -2205.197) = 1
+//  aeabi_fcmpun(-2205.196, 0) = 1
+
+///////////////////////////////////////////////////////////////////////////////
+//  Table 4, Standard single precision floating-point arithmetic helper functions
+
+//  single-precision division, n / d
+float  __wrap___aeabi_fdiv(float  n, float d)  { 
+    return qfp_fdiv_fast( n , d ); 
+}
+//  Unit Tests:
+//  aeabi_fdiv(2205.1969, 270.8886) = 8.140604292687105
+//  aeabi_fdiv(-2205.1969, 270.8886) = -8.140604292687105
+//  aeabi_fdiv(2205.1969, -270.8886) = -8.140604292687105
+//  aeabi_fdiv(-2205.1969, -270.8886) = 8.140604292687105
+
+float __wrap___aeabi_fadd(float a, float b) {
+    return qfp_fadd( a , b );
+}
+//  Unit Tests:
+//  aeabi_fadd(2205.1969, 270.8886) = 2476.0855
+//  aeabi_fadd(-2205.1969, 270.8886) = -1934.3083
+//  aeabi_fadd(2205.1969, -270.8886) = 1934.3083
+//  aeabi_fadd(-2205.1969, -270.8886) = -2476.0855
+
+float __wrap___aeabi_fsub(float a, float b) {
+    return qfp_fsub( a , b );
+}
+//  Unit Tests:
+//  aeabi_fsub(2205.1969, 270.8886) = 1934.3083
+//  aeabi_fsub(-2205.1969, 270.8886) = -2476.0855
+//  aeabi_fsub(2205.1969, -270.8886) = 2476.0855
+//  aeabi_fsub(-2205.1969, -270.8886) = -1934.3083
+
+float __wrap___aeabi_fmul(float a, float b) {
+    return qfp_fmul( a , b );
+}
+//  Unit Tests:
+//  aeabi_fmul(2205.1969, 270.8886) = 597362.70096534
+//  aeabi_fmul(-2205.1969, 270.8886) = -597362.70096534
+//  aeabi_fmul(2205.1969, -270.8886) = -597362.70096534
+//  aeabi_fmul(-2205.1969, -270.8886) = 597362.70096534
+
+///////////////////////////////////////////////////////////////////////////////
+//  Table 5, Standard integer to floating-point conversions
+
+
+//  integer to float C-style conversion.
+float __wrap___aeabi_i2f(int x) { 
+    return qfp_int2float(x);
+}
+
+float __wrap___aeabi_ui2f(unsigned int x) { 
+    return qfp_uint2float(x);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  Table 6, Standard floating-point to integer conversions
+
+//  float to integer C-style conversion. "z" means round towards 0.
+int __wrap___aeabi_f2iz(float x) { 
+    if (qfp_fcmp(x, 0) == 0) { return 0; }
+    //  qfp_float2int() works like floor().  If x is negative, we add 1 to the result.
+    int xfloored = qfp_float2int(x);
+    if (xfloored < 0) { return xfloored + 1; }
+    return xfloored; 
+}
+//  Unit Tests:
+//  aeabi_f2iz(0) = 0
+//  aeabi_f2iz(2205.1969) = 2205
+//  aeabi_f2iz(-2205.1969) = -2205
+
+//  float to unsigned C-style conversion. "z" means round towards 0.
+unsigned __wrap___aeabi_f2uiz(float x) { 
+    if (qfp_fcmp(x, 0) == 0) { return 0; }
+    if (qfp_fcmp(x, 0) < 0) { return 0; }
+    return qfp_float2uint(x); 
+}
+//  Unit Tests:
+//  aeabi_f2iz(0) = 0
+//  aeabi_f2uiz(2205.1969) = 2205
+//  aeabi_f2uiz(-2205.1969) = 0
